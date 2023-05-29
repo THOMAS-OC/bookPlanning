29d5b3e06c246b7503126ad11ba41995fee0f70a
= <=>
2023-05-29 07:57:10 +0200

    reset css et création dynamique de div

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let btn = document.querySelector(".btn")
+let nameBook = document.querySelector("input")
+let section = document.querySelector("section")
 let date = new Date();
 
 function dateAddDays(a, b) {
@@ -14,7 +16,12 @@ function dateAddDays(a, b) {
 }
 
 btn.addEventListener("click", () => {
-    alert(dateAddDays(21))
+    localStorage.setItem(nameBook.value, dateAddDays(21))
+    // crée un nouvel élément div
+    var newDiv = document.createElement("div");
+    // ajoute le nœud texte au nouveau div créé
+    section.appendChild(newDiv);
+    newDiv.innerHTML= "<p> toto </p>"
 })
 
 localStorage.setItem("Histoire de pouvoir", "date")
